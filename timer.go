36000032e819c99e027939e91f925819a46b6527
aholic <ruochen.xu@gmail.com>
2015-09-03 21:16:01 +0800

    implementation of ggtimer and testcase

---
+++ timer.go
@@ -0,0 +1,38 @@
+package timer
+
+import "time"
+
+type TimeCallbackFunc func(time.Time)
+
+func NewTicker(d time.Duration, f TimeCallbackFunc) chan bool {
+	done := make(chan bool, 1)
+	go func() {
+		t := time.NewTicker(d)
+		defer t.Stop()
+
+		for {
+			select {
+			case now := <-t.C:
+				f(now)
+			case <-done:
+				return
+			}
+		}
+	}()
+	return done
+}
+
+func NewTimer(d time.Duration, f TimeCallbackFunc) chan bool {
+	done := make(chan bool, 1)
+	go func() {
+		t := time.NewTimer(d)
+		defer t.Stop()
+		select {
+		case now := <-t.C:
+			f(now)
+		case <-done:
+			return
+		}
+	}()
+	return done
+}
---
+++ timer_test.go
@@ -0,0 +1,65 @@
+package timer
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestTimer(t *testing.T) {
+	lck := &sync.Mutex{}
+	cnt := 0
+
+	tmr := NewTimer(5*time.Second, func(time.Time) {
+		lck.Lock()
+		defer lck.Unlock()
+		cnt++
+	})
+
+	time.Sleep(1 * time.Second)
+	close(tmr)
+	if cnt != 0 {
+		t.Errorf("expect: %v, actual: %v", 0, cnt)
+	}
+
+	cnt = 0
+	tmr = NewTimer(1*time.Second, func(time.Time) {
+		lck.Lock()
+		defer lck.Unlock()
+		cnt++
+	})
+	time.Sleep(3 * time.Second)
+	close(tmr)
+	if cnt != 1 {
+		t.Errorf("expect: %v, actual: %v", 1, cnt)
+	}
+}
+
+func TestTicker(t *testing.T) {
+	lck := &sync.Mutex{}
+	cnt := 0
+
+	tmr := NewTicker(5*time.Second, func(time.Time) {
+		lck.Lock()
+		defer lck.Unlock()
+		cnt++
+	})
+
+	time.Sleep(1 * time.Second)
+	close(tmr)
+	if cnt != 0 {
+		t.Errorf("expect: %v, actual: %v", 0, cnt)
+	}
+
+	cnt = 0
+	tmr = NewTicker(1*time.Second, func(time.Time) {
+		lck.Lock()
+		defer lck.Unlock()
+		cnt++
+	})
+	time.Sleep(7 * time.Second)
+	close(tmr)
+	if cnt <= 5 {
+		t.Errorf("expect: %v, actual: %v", ">5", cnt)
+	}
+}
